09f0eaefe265cd5e4f64f32f9e3e4ad6b5615328
Vasu-Chourasia <vdevwork1906@gmail.com>
2026-03-20 22:18:18 +0530

    Added Predecessor and Successor

---
+++ 09-Trees/BST/PredecessorAndSuccessor.java
@@ -0,0 +1,51 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+    Node(int x) {
+        data = x;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    public ArrayList<Node> findPreSuc(Node root, int key) {
+        Node pre = null;
+        Node suc = null;
+        Node curr = root;
+        while(curr != null){
+            if(curr.data < key){
+                pre = curr;
+                curr = curr.right;
+            }
+            else if(curr.data > key){
+                suc = curr;             
+                curr = curr.left;
+            }
+            else{
+                if(curr.left != null){
+                    Node temp = curr.left;
+                    while(temp.right != null){
+                        temp = temp.right;
+                    }
+                    pre = temp;
+                }
+                if(curr.right != null){
+                    Node temp = curr.right;
+                    while(temp.left != null){
+                        temp = temp.left;
+                    }
+                    suc = temp;
+                }
+                break;
+            }
+        }
+        
+        ArrayList<Node> res = new ArrayList<>();
+        res.add(pre);
+        res.add(suc);
+        
+        return res;
+    }
+}
