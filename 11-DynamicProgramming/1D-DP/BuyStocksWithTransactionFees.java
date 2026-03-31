05970c007ecf187779992498ea90189655885fcd
Vasu-Chourasia <vdevwork1906@gmail.com>
2026-03-31 22:53:28 +0530

    Create BuyStocksWithTransactionFees.java

---
+++ 11-DynamicProgramming/1D-DP/BuyStocksWithTransactionFees.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int maxProfit(int[] arr,int k){
+        int n=arr.length;
+        int hold=-arr[0];
+        int cash=0;
+
+        for(int i=1;i<n;i++){
+            int prevCash=cash;
+            cash=Math.max(cash,hold+arr[i]-k);
+            hold=Math.max(hold,prevCash-arr[i]);
+        }
+
+        return cash;
+    }
+}
