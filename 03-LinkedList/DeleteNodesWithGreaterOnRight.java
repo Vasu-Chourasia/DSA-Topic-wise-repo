c91464d21ccaddd7d488212adf702d586dcbbf34
Vasu-Chourasia <vdevwork1906@gmail.com>
2026-06-08 18:48:06 +0530

    Create DeleteNodesWithGreaterOnRight.java

---
+++ 03-LinkedList/DeleteNodesWithGreaterOnRight.java
@@ -0,0 +1,38 @@
+class Solution {
+    Node compute(Node head) {
+        head = reverse(head);
+        
+        Node curr = head;
+        int max = curr.data;
+        
+        while (curr != null && curr.next != null)
+        {
+            if (curr.next.data < max)
+            {
+                curr.next = curr.next.next;
+            }
+            else
+            {
+                curr = curr.next;
+                max = curr.data;
+            }
+        }
+        
+        return reverse(head);
+    }
+    
+    Node reverse(Node head) {
+        Node prev = null;
+        Node curr = head;
+        
+        while (curr != null)
+        {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        
+        return prev;
+    }
+}
