ea6cbd5e54ebd687519cfdceef17056d1170cf2b
Vasu-Chourasia <vdevwork1906@gmail.com>
2026-07-02 21:50:07 +0530

    Create Check Subset sum divisible by k.java

---
+++ 11-DynamicProgramming/1D-DP/Check Subset sum divisible by k.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean divisibleByK(int[] arr, int k) {
+        boolean[] dp = new boolean[k];
+
+        for (int num : arr) {
+            boolean[] next = dp.clone();
+
+            next[num % k] = true;
+
+            for (int r = 0; r < k; r++) {
+                if (dp[r])
+                    next[(r + num) % k] = true;
+            }
+
+            dp = next;
+
+            if (dp[0])a
+                return true;
+        }
+
+        return false;
+    }
+}
