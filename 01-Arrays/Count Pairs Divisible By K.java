f69e179ca65e6adaaece7498818ff283f73c424b
Vasu-Chourasia <vdevwork1906@gmail.com>
2026-07-09 22:23:53 +0530

    Create Count Pairs Divisible By K.java

---
+++ 01-Arrays/Count Pairs Divisible By K.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countKdivPairs(int[] arr, int k) {
+        int[] freq = new int[k];
+        int count = 0;
+
+        for (int num : arr) {
+            int rem = num % k;
+            int complement = (k - rem) % k;
+
+            count += freq[complement];
+            freq[rem]++;
+        }
+
+        return count;
+    }
+}
