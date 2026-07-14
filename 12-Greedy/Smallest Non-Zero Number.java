d723794938db4c33a089371562f8cd38f4529912
Vasu-Chourasia <vdevwork1906@gmail.com>
2026-07-14 23:46:55 +0530

    Create Smallest Non-Zero Number.java

---
+++ 12-Greedy/Smallest Non-Zero Number.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int find(int[] arr) {
+        int need = 0;
+        for (int i = arr.length - 1; i >= 0; i--)
+            need = (need + arr[i] + 1) / 2;
+        return need;
+    }
+}
